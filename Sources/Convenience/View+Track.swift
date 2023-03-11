8b964e8859054d7ddedb02f2386ef0a9958209aa
Philipp Woessner <pwoessner17@gmail.com>
2023-03-11 12:18:46 +0100

    config changes and convenience

---
+++ Sources/Convenience/View+Track.swift
@@ -0,0 +1,32 @@
+//
+//  View+Track.swift
+//  
+//
+//  Created by Philipp Woessner on 11.03.23.
+//
+
+import SwiftUI
+
+public extension View {
+    public func track(_ scene: String) -> some View {
+        modifier(AnalyticsModifier(scene))
+    }
+}
+
+public struct AnalyticsModifier: ViewModifier {
+    private let scene: String
+
+    public init(_ scene: String) {
+        self.scene = scene
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .onAppear {
+                PWAnalytics.shared.scene(event: .shown, on: scene)
+            }
+            .onDisappear {
+                PWAnalytics.shared.scene(event: .dismissed, on: scene)
+            }
+    }
+}
---
+++ Sources/PWAnalytics/PWAnalyticsInfoPlist.swift
@@ -0,0 +1,39 @@
+//
+//  PWAnalyticsInfoPlist.swift
+//  
+//
+//  Created by Philipp Woessner on 11.03.23.
+//
+
+import Foundation
+
+public struct PWAnalyticsInfoPlist {
+    public enum Key: String {
+        case analyticsHost = "ANALYTICS_HOST"
+        case analyticsKey = "ANALYTICS_KEY"
+        case environment = "ENVIRONMENT"
+    }
+
+    private let bundle: Bundle
+
+    public init(bundle: Bundle) {
+        self.bundle = bundle
+    }
+
+    public func infoString(for key: Key) -> String? {
+        bundle.object(forInfoDictionaryKey: key.rawValue) as? String
+    }
+
+    public var environment: PWAnalyticsEnv? {
+        guard let rawValue = infoString(for: .environment) else {
+            return nil
+        }
+
+        return PWAnalyticsEnv(rawValue: rawValue)
+    }
+}
+
+public enum PWAnalyticsEnv: String {
+    case debug = "DEBUG"
+    case release = "RELEASE"
+}
